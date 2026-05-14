89c9771c8e70a777241ed28e9caa422e752d957a
JuanBernardo <uribejuanb@javeriana.edu.co>
2026-05-14 13:25:58 -0500

    fix: capa repository error en el excel

--- repository/dataRepository.py
+++ repository/dataRepository.py
@@ -1,22 +1,31 @@
 import pandas as pd
 from pathlib import Path
 
-#Agarro los datos y no los hardcodeo por simple hecho que voy a subir esto a github
+#Agarro los datos desde el excel y no los hardcodeo por simple hecho que voy a subir esto a github
 #y que es una buena práctica, más si alguna empresa revisa mis repositorios
 
 
 #lee archivo 
 def leerArchivo(nombreArchivo) :
 
-    #indicar directorio actual
-    directorioActual = Path(__file__).parent
+    try:
+        #indicar directorio actual
+        directorioActual = Path(__file__).parent
 
-    #subir al nivel del directorio principal
-    directorioPrincipal = directorioActual.parent / "datosAnios.xlsx"
+        #subir al nivel del directorio principal
+        directorioPrincipal = directorioActual.parent / "datosAnios.xlsx"
 
-    datosExcel  = pd.read_excel(directorioPrincipal, sheet_name = 18, header = 2)
+        datosExcel  = pd.read_excel(directorioPrincipal, sheet_name = 18, header = 1)
 
-    print(datosExcel)
+        print(datosExcel)
+
+        return datosExcel
+
+    except FileNotFoundError as e:
+        print(f"no se encontró el archivo {nombreArchivo} con el error e: {e}")
+    except Exception as e :
+        print(f"Ocurrió un error: {e}")
+
 
 if __name__ == "__main__" :
-    leerArchivo("datosAnios")
+    leerArchivo("datosAnios.xlsx")
